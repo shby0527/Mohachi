0322a1644d3057c28c9074e59a7375c750f0258f
HayamaUmi <umi@hayama.cf>
2016-04-14 22:12:42 +0800

    Convert to MVC5

--- test/Test/Program.cs
+++ test/Test/Program.cs
@@ -12,6 +12,7 @@ namespace Test
 			plug.Password = Console.ReadLine ();
 			byte[] testo = plug.GetBinary;
 			Console.WriteLine("加密计算后的密文:\n" + BitConverter.ToString(testo).Replace("-",""));
+			Console.WriteLine ("加密后对象长度{0}字节", testo.Length);
 			Console.WriteLine ("加密算法版本Hash：\n" + AbsPassword.GetCryptoVersion (testo));
 			Console.WriteLine ("最后验证测试："+plug.Check (testo));
 		}
